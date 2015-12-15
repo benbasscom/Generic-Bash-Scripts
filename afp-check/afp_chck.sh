1c9e73d66ba660723541c374dcf91c16f5acbcf2
Ben Bass <Ben.Bass@jhuapl.edu>
2015-12-15 12:58:39 -0500

    afp_chck checks if AFP is running and will start it if it is not.  Scheduling to run every 300 seconds.

---
+++ afp-check/afp_chck.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# afp_chck.sh
+# Version 0.2
+# Creation Date 2015-04-10
+# Last Modified Date 2015-12-15
+#
+# Created by Blake Robertson on 4/10/15.
+# Script for OS X Server to check the AFP service and start it if it is not running
+# Updated by Ben Bass on 12/15/2015
+# 
+###############################################
+#
+# Road Map of the script.
+#
+###############################################
+# 00 Header
+# Contains variables, functions and this road map.
+#
+#### 
+# 01 Script Begins
+# Run functions and check if afp is running and restart if not.
+# 
+###############################################
+# 00a
+# Define Functions
+#
+###############################################
+
+rootcheck (){
+if [ "`/usr/bin/whoami`" != "root" ] ; then
+  /bin/echo "script must be run as root"
+  exit 0
+fi
+}
+
+###############################################
+
+####################################
+# 00d
+# Set some variables and capture Std in and Std error
+#
+####################################
+
+LOGFOLDER="/Library/Logs/companyname/"
+log="/Library/Logs/companyname/afprefresh.log"
+err_log="/Library/Logs/companyname/afprefresh-err.log"
+
+# Check if the log folder exists, if not create and chmod it.
+if [ ! -d "$LOGFOLDER" ];
+then
+	mkdir -p "$LOGFOLDER"
+	chmod 755 "$LOGFOLDER"
+fi
+# Capture std err and std out to the log files above.
+exec 1>> "${log}" 
+exec 2>> "${err_log}"
+
+
+when="$(date +%Y-%m-%d-%H:%M:%S)"
+afp_status="$(ps awx | grep -q AppleFileServer ; echo $?)"
+
+
+####################################
+# 01
+# Begin Script 
+# Run functions and check if afp is running and restart if not.
+####################################
+
+rootcheck
+
+
+# echo out to the log date and time and the current status of AFP
+echo "On "$(date)", the afp status is ""$CURRENT_STATUS"
+
+
+# Check the afp status
+if [ "$CURRENT_STATUS" == 1 ] ; then
+
+	echo "afp is not running, starting at ""$when"
+    serveradmin start afp
+    echo "AFP Service started on" "$when"
+else
+
+    echo "AFP Service is running properly at ""$when"
+
+fi
+
+exit 0
